950b5e27015eda04a683d3d3e4cafb35f7b61d28
Alex <loggan94@gmail.com>
2018-06-22 18:49:34 +0800

    asses

--- node10orm/index.js
+++ node10orm/index.js
@@ -6,14 +6,24 @@ var connection = new Sequelize('test', 'root', '', {
     insecureAuth: true
 });
 
-var Article = connection.define('article', {
-    title: Sequelize.STRING,
-    body: Sequelize.TEXT
-});
+connection
+  .authenticate()
+  .then(() => {
+            console.log('Connection has been established successfully.');
+            var Article = connection.define('article', {
+                title: Sequelize.STRING,
+                body: Sequelize.TEXT
+            });
+            
+            connection.sync().then(function(){
+                Article.create({
+                    title: 'demo title',
+                    body: 'just a castaway, an island lost at sea, another lonely day with no one here but me oh, more loneliness than any man could bear rescue me before I fall into despair'
+                });
+            });
+        })
+  .catch(err => {
+            console.error('Unable to connect to the database:', err);
+  });
 
-connection.sync().then(function(){
-    Article.create({
-        title: 'demo title',
-        body: 'just a castaway, an island lost at sea, another lonely day with no one here but me oh, more loneliness than any man could bear rescue me before I fall into despair'
-    });
-});
+
--- node9/index.js
+++ node9/index.js
@@ -1,10 +1,15 @@
+// We get the functions module
 var antminer = require('antminer-func');
+
+// Here comes the ORM
+/* const Sequelize = require('sequelize');
+const connection = new Sequelize("mydb", "root", "");
 var Sequelize = require('sequelize');
 var connection = new Sequelize('test', 'root', '', {
     host: '127.0.0.1',
     dialect: 'mysql',
     insecureAuth: true
-});
+}); */
 
 
 var fs = require('fs');
@@ -18,6 +23,5 @@ fs.writeFile(__dirname+"/info.txt", "", function (wrt_err){
     if(wrt_err) throw wrt_err;
 });
 
-
 antminer.readOverView(urls2);
-antminer.readStats(urls2);
+//antminer.readStats(urls2);
